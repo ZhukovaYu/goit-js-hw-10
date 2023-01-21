46071b470935dd254ce22468c589036da48317c6
ZhukovaYu <zhukova.julia@gmail.com>
2023-01-21 17:45:33 +0100

    goit-js-hw-10

--- src/index.js
+++ src/index.js
@@ -5,3 +5,77 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
+Notify.init({});
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchBox.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
+
+function onSearchInput(e) {
+  const countryName = e.target.value.trim();
+  if (countryName === '') {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
+  }
+
+fetchCountries(countryName)
+    .then(markup)
+    .catch(() => {
+      Notify.failure('Oops, there is no country with that name');
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+    });
+}
+
+function markup(countries) {
+  const countOfCountries = countries.length;
+  if (countOfCountries > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+  if (countOfCountries <= 10 && countOfCountries >= 2) {
+    createCountryList(countries);
+    return;
+  }
+  if (countOfCountries === 1) {
+   createCountryCard(countries);
+  }
+}
+
+function createCountryList(countries) {
+  const listOfCountries = countries
+    .map(
+      ({ name: { official: officialName }, flags: { svg: flag } }) =>
+        `<li class="country-name">
+            <img src="${flag}" alt="${officialName}" width="30" />
+            <span>${officialName}</span>
+        </li>`
+    )
+    .join('');
+  refs.countryList.innerHTML = listOfCountries;
+}
+
+function createCountryCard([
+  { name: { official: officialName },
+    capital,
+    population,
+    flags: { svg: flag },
+    languages,
+  },
+]) {
+    refs.countryList.innerHTML = '';
+    const listOfLangs = Object.values(languages).join(', ');
+    refs.countryInfo.innerHTML = `
+    <div class="country-name country-name--highlight">
+        <img src="${flag}" alt="${officialName}" width="50" />
+        <span>${officialName}</span>
+    </div>
+    <div><strong>Capital: </strong><span>${capital}</span></div>
+    <div><strong>Population: </strong><span>${population}</span></div>
+    <div><strong>Languages: </strong><span>${listOfLangs}</span></div>`;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://restcountries.com';
 const FIELDS = 'name,capital,population,flags,languages';
 
-export function fetchCountries(name) {
+export function fetchCountries(countryName) {
   return fetch(
-    `${BASE_URL}/v3.1/name/${name}?fields=${FIELDS}`
+    `${BASE_URL}/v3.1/name/${countryName}?fields=${FIELDS}`
     ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
